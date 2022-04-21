74d332c9ae1cae6f33735a44d628b453236874dd
zuzanapiorkowska <napiorkowskaz@gmail.com>
2022-04-21 14:59:35 +0200

    change thing

--- packages/got-front/pages/index.tsx
+++ packages/got-front/pages/index.tsx
@@ -4,7 +4,7 @@ import { Button } from '../components/Button';
 const Game = () => {
   return (
     <>
-      <h1>----GAME OF LIFE----</h1>
+      <h1>---GAME OF LIFE---</h1>
       <div className="container">
         <GOTBoard />
       </div>
